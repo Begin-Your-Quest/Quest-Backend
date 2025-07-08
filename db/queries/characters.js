987f34cae4c990148a441c37fae7b9e13ed263de
John Nabers <jmn17sw@gmail.com>
2025-07-08 10:57:01 -0500

    adjusted invoked variables in create charaters function

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -1,11 +1,11 @@
 import client from '../client.js'
-export const CreateCharacters = async (characterName) => {
+export const createCharacters = async (name, clas,attack, health, userId ) => {
   const sql = `
   INSERT INTO chracter (name, class, attack, health, user_id)
   VALUES ($1, $2, $3, $4, $5)
   RETURNING *;
   `;
 
-  const { rows: [createdCharacter] } = await client.query(sql, [characterName])
+  const { rows: [createdCharacter] } = await client.query(sql, [name, clas, attack, health, userId])
   return createdCharacter
 }
