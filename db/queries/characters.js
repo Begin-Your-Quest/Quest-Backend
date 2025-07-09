6e5977cfef8ec3146f9ac298fd353d56450459a6
Zachary Devine <zach.devine17@gmail.com>
2025-07-09 08:12:03 -0700

    modified the /put router to use and update function, wrote the character update function.

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -19,3 +19,9 @@ export const getCharacterByCharacterId = async (characterId) => {
   const {rows: [character]} = await db.query(sql, [characterId]);
   return character;
 };
+
+export const updateCharacter = async (name,clas,attack,health,userId) => {
+  const sql = `update characters set name=$1, class=$2, attack_stat=$3, health_stat=$4 where user_id=$5`;
+  const {rows: [character]} = await db.query(sql,[name,clas,attack,health,userId]);
+  return character;
+};
