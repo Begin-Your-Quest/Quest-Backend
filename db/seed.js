9a425bf1ceb47736ba394c2e7bbbb8694b3fe5fe
John Nabers <jmn17sw@gmail.com>
2025-07-14 15:03:27 -0500

    resolved seeding conflicts in skills.js; resolved seeing conflicts in users.js

--- db/seed.js
+++ db/seed.js
@@ -7,8 +7,8 @@ import { createSkill } from "#db/queries/skills"
 await db.connect();
 await seed();
 await db.end();
-console.log("🌱 Database seeded.");
 
+console.log('creating sessions...')
 async function seed() {
   const userOne = await createUser("username", "password");
   for (let i = 1; i <= 5; i++) {
@@ -28,20 +28,21 @@ async function seed() {
     const character = await createCharacter(`char${i}`,"mage",7,4,userTwo.id);
   }
 
+  console.log('...sessions created')
 
-  const crossSlash = await createSkill("Cross Slash", 2, 5, "A tried-&-true hack-&-slash technique.");
+console.log('creating skills...')
+  const crossSlash = await createSkill("Cross Slash", 2, 5, "A tried-and-true hack-and-slash technique.");
   const pommelStrike = await createSkill("Pommel Strike", 1, 1, "Quick attack")
   const fireBlast = await createSkill("Fire Blast", 2, 6, "unleashes an massive explosion.")
   const icicle = await createSkill("Icicle Shot", 1, 3, "A concentrated burst of piercing cold.")
   const carefulAim = await createSkill("Careful Aim", 2, 5, "Pause momentarily to deliver an expert headshot.")
   const quickShot = await createSkill("Quick Shot", 1, 2, "A rapid fire onslaught.")
-  const backstab = await createSkill("Backstab", 2, 6, "Sidle up & smile. Hits enemies where they hurt.")
+  const backstab = await createSkill("Backstab", 2, 6, "Sidle up and smile. Hits enemies where they hurt.")
   const throwingKnife = await createSkill("Throwing Knife", 1, 3, "At later levels can be coated with various poisons.")
   const cure = await createSkill("Cure", 2, 6, "Restores health to self and other users.")
   const condemn = await createSkill("Condemn", 2, 6, "Call down divine wrath to smite enemies.")
+console.log('...skills created')
 
-
-
-
+console.log("🌱 Database seeded.");
 }
 
